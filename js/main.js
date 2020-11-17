65189044721b67b2cd2a0f4d226e014c1cf52687
Code-Jones <jones.matthew@edu.sait.ca>
2020-11-17 09:49:36 -0700

    added background sounds

--- js/main.js
+++ js/main.js
@@ -1,25 +1,38 @@
+let notMute = true;
+
 function start() {
+    document.getElementById('click-to-start').style.display = 'none';
     const animated = document.querySelectorAll('.animated');
-    animated.item(0).classList.toggle('active');
     playSound('./assets/sounds/login.wav');
-    setBackgroundSounds();
+    playBackgroundSounds();
     // remove active if animation ended
-    for (let i = 0; i < animated.length ; i++) {
+    for (let i = 0; i < animated.length; i++) {
         animated[i].addEventListener('animationend', () => {
-            animated[i].classList.toggle('active');
-            console.log("animation ended");
+            // animated[i].classList.toggle('active');
+            animated[i + 1].classList.toggle('active');
         })
     }
+    let interval;
+    animated.item(0).classList.toggle('active');
+    playSound('./assets/sounds/print.wav');
+    let time = function timeout() {
+        setTimeout(function () {
+            playSound('./assets/sounds/print.wav');
+            timeout();
+        }, 1000);
+    }
+    animated.item(animated.length - 1).addEventListener('animationend', () => {
+        window.clearTimeout(time);
+        next();
+    });
+}
 
-    // document.getElementById("opening").style.display = 'block';
-    // let list = document.getElementById("opening").getElementsByTagName('p');
-    // for (const listElement of list) {
-    //     listElement.style.animationPlayState = 'running';
-    //
-    // }
-    // backgroundMusic = new Sound("./assets/sounds/hum1.wav");
-    // backgroundMusic.play();
-    document.getElementsByClassName("screen").style = "display: none;"
+
+
+function next() {
+    const animated = document.querySelectorAll('.animated');
+    
+
 }
 
 function playSound(src) {
@@ -27,7 +40,28 @@ function playSound(src) {
     audio.play();
 }
 
-(function setBackgroundSounds() {
+function playBackgroundSounds() {
+    let fan = new Audio("./assets/sounds/computer_fan.mp3");
+    let hardDrive = new Audio("./assets/sounds/ibm_hard_drive.mp3");
+    fan.play();
+    hardDrive.loop = true;
+    hardDrive.load();
+    hardDrive.play();
+}
+// jQuery(document).ready(function (){
+//     var audioArray = document.getElementsByClassName('songs');
+//     var i = 0;
+//     audioArray[i].play();
+//     for (i = 0; i < audioArray.length - 1; ++i) {
+//         audioArray[i].addEventListener('ended', function(e){
+//             var currentSong = e.target;
+//             var next = $(currentSong).nextAll('audio');
+//             if (next.length) $(next[0]).trigger('play');
+//         });
+//     }
+// });
+
+function setBackgroundSounds(notMute) {
     let alternator = 0;
     if (alternator === 0) {
         let audio = new Audio('./assets/sounds/hum1.wav');
@@ -38,8 +72,9 @@ function playSound(src) {
         audio.play();
         alternator--;
     }
-    setTimeout(setBackgroundSounds,2200);
-}())
+    setTimeout(setBackgroundSounds, 2200);
+
+}
 
 function Sound(src) {
     this.sound = document.createElement("audio");
@@ -48,10 +83,10 @@ function Sound(src) {
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
     document.body.appendChild(this.sound);
-    this.play = function(){
+    this.play = function () {
         this.sound.play();
     }
-    this.stop = function(){
+    this.stop = function () {
         this.sound.pause();
     }
 };
