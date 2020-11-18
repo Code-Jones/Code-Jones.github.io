3b7b6fc9788614c5be12fd6487712937da2c31fa
Code-Jones <jones.matthew@edu.sait.ca>
2020-11-17 20:02:54 -0700

    lots of progress + wordlists for game

--- js/main.js
+++ js/main.js
@@ -1,38 +1,128 @@
 let notMute = true;
+let printSound = new Audio('./assets/sounds/print.wav');
+let s1 = new Audio('./assets/sounds/key1.wav'); // change this
+let s2 = new Audio('./assets/sounds/key2.wav');
+let s3 = new Audio('./assets/sounds/key3.wav');
+let s4 = new Audio('./assets/sounds/key4.wav');
+let s5 = new Audio('./assets/sounds/key5.wav');
+let s6 = new Audio('./assets/sounds/key6.wav');
+let s7 = new Audio('./assets/sounds/key7.wav');
+let clickSounds = [s1, s2, s3, s4, s5, s6, s7];
+printSound.loop = true;
+let newIndex = null;
+newIndex = makeIndex(newIndex);
+let numLines = 17;
+let numPiecesAcross = 12;
+const words_10 = ["RECRUITING", "FLASHLIGHT", "BINOCULARS", "SUPPRESSOR", "DISPOSABLE", "CAMOUFLAGE", "TOOTHBRUSH", "INCENDIARY", "LIEUTENANT", "OCCUPATION", "ACTIVATING", "ENCOUNTERS", "STRETCHING", "FUTURISTIC", "APARTMENTS", "VEGETABLES", "RESTRICTED", "APOCALYPSE", "CONCERNING", "SCULPTURES", "CURRICULUM", "MECHANICAL", "THEREAFTER", "VANQUISHED", "DETERMINED", "FUNCTIONAL", "COMMISSION"];
+
 
 function start() {
-    document.getElementById('click-to-start').style.display = 'none';
+    document.getElementById('click-to-start').remove();
     const animated = document.querySelectorAll('.animated');
     playSound('./assets/sounds/login.wav');
     playBackgroundSounds();
     // remove active if animation ended
     for (let i = 0; i < animated.length; i++) {
+        animated[i].addEventListener('animationstart', () => {
+            printSound.play();
+        })
         animated[i].addEventListener('animationend', () => {
-            // animated[i].classList.toggle('active');
             animated[i + 1].classList.toggle('active');
+            printSound.pause();
         })
     }
-    let interval;
+
     animated.item(0).classList.toggle('active');
-    playSound('./assets/sounds/print.wav');
-    let time = function timeout() {
-        setTimeout(function () {
-            playSound('./assets/sounds/print.wav');
-            timeout();
-        }, 1000);
-    }
     animated.item(animated.length - 1).addEventListener('animationend', () => {
-        window.clearTimeout(time);
-        next();
+        printSound.pause();
+        setTimeout(function () {
+            next();
+        }, 3000);
     });
 }
 
 
+function replaceWithWord(pc) {
+    shuffle(words_10);
+    let p = pc.childNodes;
+    p[Math.floor(Math.random() * p.length)].childNodes[0].innerText = words_10.pop();
+}
 
+function shuffle(a) {
+    let j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+let keys;
 function next() {
-    const animated = document.querySelectorAll('.animated');
-    
+    console.log("Intro finished");
+    document.getElementById('opening').remove();
+    let gameScreen = document.createElement('div');
+    gameScreen.classList.add('gameScreen');
+    keys = document.createElement('div');
+    let keys_2 = document.createElement('div');
+    for (let i = 0; i < numLines * numPiecesAcross; i++) {
+        keys.appendChild(makeGameNodes());
+        keys_2.appendChild(makeGameNodes());
+    }
+    for (let i = 0; i < 5; i++) {
+        replaceWithWord(keys);
+        replaceWithWord(keys_2);
+    }
+    gameScreen.appendChild(makeIndexLines());
+    gameScreen.appendChild(keys);
+    let space = document.createElement('div');
+    gameScreen.appendChild(space);
+    gameScreen.appendChild(makeIndexLines());
+    gameScreen.appendChild(keys_2);
+    document.getElementById('screen').appendChild(gameScreen);
+}
 
+function makeIndexLines() {
+    let line = document.createElement('div');
+    for (let i = 0; i < numLines; i++) {
+        let index = document.createElement('p');
+        index.classList.add('index');
+        index.innerText = makeIndex(newIndex);
+        line.append(index);
+    }
+    return line;
+}
+
+
+function makeGameNodes() {
+    const extra = ["{", "}", "[", "]", "'", ":", ";", "/", "?", ".", ",", "<", ">", "(", ")", "-", "_", "*", "&", "^", "%", "$", "#", "@", "!", "~", "`", "|", "\\", "="];
+    let pieces = document.createElement('p');
+    pieces.classList.add('piece');
+    let span = document.createElement('span');
+    span.innerText = extra[Math.floor(Math.random() * extra.length)];
+    span.addEventListener('mouseover', function (e) {
+        clickSounds[Math.floor(Math.random() * clickSounds.length)].play();
+    });
+    pieces.appendChild(span);
+    return pieces;
+}
+
+
+function makeIndex(line) {
+    if (line === null) {
+        let line = "0x" + Math.random().toString(36).substring(2, 3).toUpperCase() + Math.random().toString(36).substring(2, 3).toUpperCase();
+        for (let i = 0; i < 2; i++) {
+            line += Math.random().toString(36).substring(2, 3).toUpperCase();
+        }
+        return line;
+    } else {
+        let temp = line.split("");
+        temp[4] = Math.random().toString(36).substring(2, 3).toUpperCase();
+        temp[5] = Math.random().toString(36).substring(2, 3).toUpperCase();
+        line = temp.join("");
+        return line;
+    }
 }
 
 function playSound(src) {
@@ -48,46 +138,5 @@ function playBackgroundSounds() {
     hardDrive.load();
     hardDrive.play();
 }
-// jQuery(document).ready(function (){
-//     var audioArray = document.getElementsByClassName('songs');
-//     var i = 0;
-//     audioArray[i].play();
-//     for (i = 0; i < audioArray.length - 1; ++i) {
-//         audioArray[i].addEventListener('ended', function(e){
-//             var currentSong = e.target;
-//             var next = $(currentSong).nextAll('audio');
-//             if (next.length) $(next[0]).trigger('play');
-//         });
-//     }
-// });
 
-function setBackgroundSounds(notMute) {
-    let alternator = 0;
-    if (alternator === 0) {
-        let audio = new Audio('./assets/sounds/hum1.wav');
-        audio.play();
-        alternator++;
-    } else {
-        let audio = new Audio('./assets/sounds/hum2.wav');
-        audio.play();
-        alternator--;
-    }
-    setTimeout(setBackgroundSounds, 2200);
-
-}
-
-function Sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function () {
-        this.sound.play();
-    }
-    this.stop = function () {
-        this.sound.pause();
-    }
-};
 
