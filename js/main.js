a5409b1443ba102b25e17604f09717b19e97da69
Code-Jones <jones.matthew@edu.sait.ca>
2020-11-29 13:49:55 -0700

    small changes better doe

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-let notMute = true;
 let printSound = new Audio('./assets/sounds/print.wav');
 let s1 = new Audio('./assets/sounds/key1.wav'); // change this
 let s2 = new Audio('./assets/sounds/key2.wav');
@@ -14,7 +13,11 @@ newIndex = makeIndex(newIndex);
 let numLines = 17;
 let numPiecesAcross = 12;
 const words_10 = ["RECRUITING", "FLASHLIGHT", "BINOCULARS", "SUPPRESSOR", "DISPOSABLE", "CAMOUFLAGE", "TOOTHBRUSH", "INCENDIARY", "LIEUTENANT", "OCCUPATION", "ACTIVATING", "ENCOUNTERS", "STRETCHING", "FUTURISTIC", "APARTMENTS", "VEGETABLES", "RESTRICTED", "APOCALYPSE", "CONCERNING", "SCULPTURES", "CURRICULUM", "MECHANICAL", "THEREAFTER", "VANQUISHED", "DETERMINED", "FUNCTIONAL", "COMMISSION"];
-
+const duds = ["[<>]", "[<>]", "[<>]", "[<>]", "[<>]", "[<>]", "[<>]", "[<>]", "[<>]", "[<>]", "[<>]", "[<>]", "[<>]"]
+let taken = [];
+let takenCounter;
+let keys;
+let key;
 
 function start() {
     document.getElementById('click-to-start').remove();
@@ -24,9 +27,10 @@ function start() {
     // remove active if animation ended
     for (let i = 0; i < animated.length; i++) {
         animated[i].addEventListener('animationstart', () => {
-            printSound.play();
+            printSound.play().then(r => (console.log(r + " sounds played")));
         })
         animated[i].addEventListener('animationend', () => {
+            // animated[i + 1].classList.add('active');
             animated[i + 1].classList.toggle('active');
             printSound.pause();
         })
@@ -41,11 +45,93 @@ function start() {
     });
 }
 
+function next() {
+    console.log("Intro finished");
+    document.getElementById('opening').remove();
+    let gameScreen = document.createElement('div');
+    gameScreen.classList.add('gameScreen');
+    keys = document.createElement('div');
+    let keys_2 = document.createElement('div');
+    for (let i = 0; i < numLines * numPiecesAcross + 170; i++) {
+        keys.appendChild(makeGameNodes());
+        keys_2.appendChild(makeGameNodes());
+    }
+    key = keys.childNodes;
+    let key_2 = keys_2.childNodes;
+    // for (let i = 0; i < numLines * numPiecesAcross; i++) {
+    //     key[i].addEventListener('animationstart', () => {
+    //         printSound.play();
+    //     })
+    //     key[i].addEventListener('animationend', () => {
+    //         key[i + 1].classList.toggle('active');
+    //         printSound.pause();
+    //     })
+    //     key_2[i].addEventListener('animationstart', () => {
+    //         printSound.play();
+    //     })
+    //     key_2[i].addEventListener('animationend', () => {
+    //         key_2[i + 1].classList.toggle('active');
+    //         printSound.pause();
+    //     })
+    // }
+    let keysChildren = keys.childNodes;
+    let keys_2Children = keys_2.childNodes;
+    for (let i = 0; i < 6; i++) {
+        replaceWithFiller(keysChildren, "word");
+        replaceWithFiller(keysChildren, "dud");
+        replaceWithFiller(keys_2Children, "word");
+        replaceWithFiller(keys_2Children, "dud");
+    }
+    gameScreen.appendChild(makeIndexLines());
+    gameScreen.appendChild(keys);
+    let space = document.createElement('div');
+    gameScreen.appendChild(space);
+    gameScreen.appendChild(makeIndexLines());
+    gameScreen.appendChild(keys_2);
+    document.getElementById('screen').appendChild(gameScreen);
 
-function replaceWithWord(pc) {
-    shuffle(words_10);
-    let p = pc.childNodes;
-    p[Math.floor(Math.random() * p.length)].childNodes[0].innerText = words_10.pop();
+    keys[0].classList.toggle('active');
+    keys_2[0].classList.toggle('active');
+    keys.item(keys.length - 1).addEventListener('animationend', () => {
+        printSound.pause();
+        // setTimeout(function () {
+        //     next();
+        // }, 3000);
+    });
+}
+
+function makeIndexLines() {
+    let line = document.createElement('div');
+    for (let i = 0; i < numLines; i++) {
+        let index = document.createElement('p');
+        index.innerText = makeIndex(newIndex);
+        line.append(index);
+    }
+    return line;
+}
+
+function replaceWithFiller(children, type) {
+    let randomIndex = Math.floor(Math.random() * children.length);
+    while (taken.includes(randomIndex)) { // stop having two fillers right beside each other
+        randomIndex = Math.floor(Math.random() * children.length);
+    }
+    takenCounter += 2;
+    taken[takenCounter] = randomIndex;
+    taken[takenCounter + 1] = randomIndex + 1;
+    children[randomIndex].childNodes[0].innerText = type === "word" ? words_10.pop() : duds.pop();
+}
+
+function makeGameNodes() {
+    const extra = ["{", "}", "[", "]", "'", ":", ";", "/", "?", ".", ",", "<", ">", "(", ")", "-", "_", "*", "&", "^", "%", "$", "#", "@", "!", "~", "`", "|", "\\", "="];
+    let piece = document.createElement('p');
+    piece.classList.add('piece');
+    let span = document.createElement('span');
+    span.innerText = extra[Math.floor(Math.random() * extra.length)];
+    span.addEventListener('mouseover', function () {
+        clickSounds[Math.floor(Math.random() * clickSounds.length)].play();
+    });
+    piece.appendChild(span);
+    return piece;
 }
 
 function shuffle(a) {
@@ -58,56 +144,6 @@ function shuffle(a) {
     }
     return a;
 }
-let keys;
-function next() {
-    console.log("Intro finished");
-    document.getElementById('opening').remove();
-    let gameScreen = document.createElement('div');
-    gameScreen.classList.add('gameScreen');
-    keys = document.createElement('div');
-    let keys_2 = document.createElement('div');
-    for (let i = 0; i < numLines * numPiecesAcross; i++) {
-        keys.appendChild(makeGameNodes());
-        keys_2.appendChild(makeGameNodes());
-    }
-    for (let i = 0; i < 5; i++) {
-        replaceWithWord(keys);
-        replaceWithWord(keys_2);
-    }
-    gameScreen.appendChild(makeIndexLines());
-    gameScreen.appendChild(keys);
-    let space = document.createElement('div');
-    gameScreen.appendChild(space);
-    gameScreen.appendChild(makeIndexLines());
-    gameScreen.appendChild(keys_2);
-    document.getElementById('screen').appendChild(gameScreen);
-}
-
-function makeIndexLines() {
-    let line = document.createElement('div');
-    for (let i = 0; i < numLines; i++) {
-        let index = document.createElement('p');
-        index.classList.add('index');
-        index.innerText = makeIndex(newIndex);
-        line.append(index);
-    }
-    return line;
-}
-
-
-function makeGameNodes() {
-    const extra = ["{", "}", "[", "]", "'", ":", ";", "/", "?", ".", ",", "<", ">", "(", ")", "-", "_", "*", "&", "^", "%", "$", "#", "@", "!", "~", "`", "|", "\\", "="];
-    let pieces = document.createElement('p');
-    pieces.classList.add('piece');
-    let span = document.createElement('span');
-    span.innerText = extra[Math.floor(Math.random() * extra.length)];
-    span.addEventListener('mouseover', function (e) {
-        clickSounds[Math.floor(Math.random() * clickSounds.length)].play();
-    });
-    pieces.appendChild(span);
-    return pieces;
-}
-
 
 function makeIndex(line) {
     if (line === null) {
